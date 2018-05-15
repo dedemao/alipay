57b464c20d70db1c0514091506ddb1f8836701f7
dedemao <admin@dedemao.com>
2018-05-15 14:06:55 +0800

    交易结算（分账、分润）

---
+++ settle.php
@@ -0,0 +1,210 @@
+<?php
+header('Content-type:text/html; Charset=utf-8');
+/*** 请填写以下配置信息 ***/
+$appid = '';    //https://open.alipay.com 账户中心->密钥管理->开放平台密钥，填写开通了“当面付”应用的APPID
+$outTradeNo = uniqid();         //结算请求流水号 开发者自行生成并保证唯一性
+$tradeNo = '';     //支付宝订单号
+$tradeOut = '';     //分账支出方账户，本参数为要分账的支付宝账号对应的支付宝唯一用户号。以2088开头的纯16位数字。
+$tradeIn = '';     //分账收入方账户，本参数为要分账的支付宝账号对应的支付宝唯一用户号。以2088开头的纯16位数字。
+$amount = 0.01;                      //分账金额。即收款方实际收到的金额
+$signType = 'RSA2';       //签名算法类型，支持RSA2和RSA，推荐使用RSA2
+//商户私钥，填写对应签名算法类型的私钥，如何生成密钥参考：https://docs.open.alipay.com/291/105971和https://docs.open.alipay.com/200/105310
+$rsaPrivateKey='';
+/*** 配置结束 ***/
+$aliPay = new AlipayService();
+$aliPay->setAppid($appid);
+$aliPay->setRsaPrivateKey($rsaPrivateKey);
+$aliPay->setTotalFee($amount);
+$aliPay->setOutTradeNo($outTradeNo);
+$aliPay->setTradeNo($tradeNo);
+$aliPay->setTranOut($tradeOut);
+$aliPay->setTranIn($tradeIn);
+$result = $aliPay->doSettle();
+$result = $result['alipay_trade_order_settle_response'];
+if($result['code'] && $result['code']=='10000'){
+    echo '分账成功';
+}else{
+    echo $result['msg'].' : '.$result['sub_msg'];
+}
+class AlipayService
+{
+    protected $appId;
+    protected $charset;
+    protected $notifyUrl;
+    //私钥值
+    protected $rsaPrivateKey;
+    protected $totalFee;
+    protected $outTradeNo;
+    protected $orderName;
+
+    protected $tradeNo;
+    protected $tranOut;
+    protected $tranIn;
+    protected $amountPercentage;
+
+    public function __construct()
+    {
+        $this->charset = 'utf8';
+    }
+
+    public function setAppid($appid)
+    {
+        $this->appId = $appid;
+    }
+
+    public function setTradeNo($tradeNo)
+    {
+        $this->tradeNo = $tradeNo;
+    }
+
+    public function setTranOut($tranOut)
+    {
+        $this->tranOut = $tranOut;
+    }
+
+    public function setTranIn($tranIn)
+    {
+        $this->tranIn = $tranIn;
+    }
+
+    public function setRsaPrivateKey($rsaPrivateKey)
+    {
+        $this->rsaPrivateKey = $rsaPrivateKey;
+    }
+
+    public function setTotalFee($payAmount)
+    {
+        $this->totalFee = $payAmount;
+    }
+
+    public function setOutTradeNo($outTradeNo)
+    {
+        $this->outTradeNo = $outTradeNo;
+    }
+
+    /**
+     * 发起分账
+     * @return array
+     */
+    public function doSettle()
+    {
+        //请求参数
+        $requestConfigs = array(
+            'out_request_no'=>$this->outTradeNo,
+            'trade_no'=>$this->tradeNo,
+            'royalty_parameters'=>array(
+                array(
+                    'trans_out'=>$this->tranOut,
+                    'trans_in'=>$this->tranIn,
+                    'amount'=>$this->totalFee,
+                    'amount_percentage'=>100,
+                    'desc'=>'分账给'.$this->tranIn,
+                )
+            ),
+            'operator_id'=>'A001',  //操作员id（选填）
+        );
+        $commonConfigs = array(
+            //公共参数
+            'app_id' => $this->appId,
+            'method' => 'alipay.trade.order.settle',             //接口名称
+            'format' => 'JSON',
+            'charset'=>$this->charset,
+            'sign_type'=>'RSA2',
+            'timestamp'=>date('Y-m-d H:i:s'),
+            'version'=>'1.0',
+            'app_auth_token'=>'',
+            'biz_content'=>json_encode($requestConfigs),
+        );
+        $commonConfigs["sign"] = $this->generateSign($commonConfigs, $commonConfigs['sign_type']);
+        $result = $this->curlPost('https://openapi.alipay.com/gateway.do',$commonConfigs);
+        $result = iconv('GBK','UTF-8',$result);
+        return json_decode($result,true);
+    }
+    public function generateSign($params, $signType = "RSA") {
+        return $this->sign($this->getSignContent($params), $signType);
+    }
+    protected function sign($data, $signType = "RSA") {
+        $priKey=$this->rsaPrivateKey;
+        $res = "-----BEGIN RSA PRIVATE KEY-----\n" .
+            wordwrap($priKey, 64, "\n", true) .
+            "\n-----END RSA PRIVATE KEY-----";
+        ($res) or die('您使用的私钥格式错误，请检查RSA私钥配置');
+        if ("RSA2" == $signType) {
+            openssl_sign($data, $sign, $res, version_compare(PHP_VERSION,'5.4.0', '<') ? SHA256 : OPENSSL_ALGO_SHA256); //OPENSSL_ALGO_SHA256是php5.4.8以上版本才支持
+        } else {
+            openssl_sign($data, $sign, $res);
+        }
+        $sign = base64_encode($sign);
+        return $sign;
+    }
+    /**
+     * 校验$value是否非空
+     *  if not set ,return true;
+     *    if is null , return true;
+     **/
+    protected function checkEmpty($value) {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+        return false;
+    }
+    public function getSignContent($params) {
+        ksort($params);
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === $this->checkEmpty($v) && "@" != substr($v, 0, 1)) {
+                // 转换成目标字符集
+                $v = $this->characet($v, $this->charset);
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+    /**
+     * 转换字符集编码
+     * @param $data
+     * @param $targetCharset
+     * @return string
+     */
+    function characet($data, $targetCharset) {
+        if (!empty($data)) {
+            $fileType = $this->charset;
+            if (strcasecmp($fileType, $targetCharset) != 0) {
+                $data = mb_convert_encoding($data, $targetCharset, $fileType);
+                //$data = iconv($fileType, $targetCharset.'//IGNORE', $data);
+            }
+        }
+        return $data;
+    }
+    public function curlPost($url = '', $postData = '', $options = array())
+    {
+        if (is_array($postData)) {
+            $postData = http_build_query($postData);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30); //设置cURL允许执行的最长秒数
+        if (!empty($options)) {
+            curl_setopt_array($ch, $options);
+        }
+        //https请求 不验证证书和host
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
