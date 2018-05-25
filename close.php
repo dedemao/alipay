6042716bc2c5d2109e800adcf52ad529224e21dc
dedemao <admin@dedemao.com>
2018-05-25 11:43:07 +0800

    交易关闭接口
    用于交易创建后，用户在一定时间内未进行支付，可调用该接口直接将未付款的交易进行关闭

---
+++ close.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * alipay.trade.close(统一收单交易关闭接口)
+ *
+ * 用于交易创建后，用户在一定时间内未进行支付，可调用该接口直接将未付款的交易进行关闭。
+ */
+header('Content-type:text/html; Charset=utf-8');
+/*** 请填写以下配置信息 ***/
+$appid = 'xxxxx';  //https://open.alipay.com 账户中心->密钥管理->开放平台密钥，填写添加了电脑网站支付的应用的APPID
+$tradeNo = '';     //在支付宝系统中的交易流水号。最短 16 位，最长 64 位。和out_trade_no不能同时为空，如果同时传了 out_trade_no和 trade_no，则以 trade_no为准。
+$outTradeNo = '';     //订单支付时传入的商户订单号,和支付宝交易号不能同时为空。 trade_no,out_trade_no如果同时存在优先取trade_no
+$signType = 'RSA2';       //签名算法类型，支持RSA2和RSA，推荐使用RSA2
+//商户私钥，填写对应签名算法类型的私钥，如何生成密钥参考：https://docs.open.alipay.com/291/105971和https://docs.open.alipay.com/200/105310
+$rsaPrivateKey='';
+/*** 配置结束 ***/
+$aliPay = new AlipayService();
+$aliPay->setAppid($appid);
+$aliPay->setRsaPrivateKey($rsaPrivateKey);
+$aliPay->setTradeNo($tradeNo);
+$aliPay->setOutTradeNo($outTradeNo);
+$result = $aliPay->doClose();
+$result = $result['alipay_trade_close_response'];
+if($result['code'] && $result['code']=='10000'){
+    echo '<h1>订单已关闭</h1>';
+}else{
+    echo $result['msg'].' : '.$result['sub_msg'];
+}
+
+class AlipayService
+{
+    protected $appId;
+    protected $returnUrl;
+    protected $notifyUrl;
+    protected $charset;
+    //私钥值
+    protected $rsaPrivateKey;
+    protected $outTradeNo;
+    protected $tradeNo;
+
+    public function __construct()
+    {
+        $this->charset = 'utf8';
+    }
+    public function setAppid($appid)
+    {
+        $this->appId = $appid;
+    }
+    public function setRsaPrivateKey($saPrivateKey)
+    {
+        $this->rsaPrivateKey = $saPrivateKey;
+    }
+    public function setOutTradeNo($outTradeNo)
+    {
+        $this->outTradeNo = $outTradeNo;
+    }
+    public function settradeNo($tradeNo)
+    {
+        $this->tradeNo = $tradeNo;
+    }
+
+    /**
+     * 关闭订单
+     * @return array
+     */
+    public function doClose()
+    {
+        //请求参数
+        $requestConfigs = array(
+            'trade_no'=>$this->tradeNo,
+            'out_trade_no'=>$this->outTradeNo,
+        );
+        $commonConfigs = array(
+            //公共参数
+            'app_id' => $this->appId,
+            'method' => 'alipay.trade.close',             //接口名称
+            'format' => 'JSON',
+            'charset'=>$this->charset,
+            'sign_type'=>'RSA2',
+            'timestamp'=>date('Y-m-d H:i:s'),
+            'version'=>'1.0',
+            'biz_content'=>json_encode($requestConfigs),
+        );
+        $commonConfigs["sign"] = $this->generateSign($commonConfigs, $commonConfigs['sign_type']);
+        $result = $this->curlPost('https://openapi.alipay.com/gateway.do',$commonConfigs);
+        $resultArr = json_decode($result,true);
+        if(empty($resultArr)){
+            $result = iconv('GBK','UTF-8//IGNORE',$result);
+            return json_decode($result,true);
+        }
+        return $resultArr;
+    }
+
+    public function generateSign($params, $signType = "RSA") {
+        return $this->sign($this->getSignContent($params), $signType);
+    }
+
+    protected function sign($data, $signType = "RSA") {
+        $priKey=$this->rsaPrivateKey;
+        $res = "-----BEGIN RSA PRIVATE KEY-----\n" .
+            wordwrap($priKey, 64, "\n", true) .
+            "\n-----END RSA PRIVATE KEY-----";
+        ($res) or die('您使用的私钥格式错误，请检查RSA私钥配置');
+        if ("RSA2" == $signType) {
+            openssl_sign($data, $sign, $res, version_compare(PHP_VERSION,'5.4.0', '<') ? SHA256 : OPENSSL_ALGO_SHA256); //OPENSSL_ALGO_SHA256是php5.4.8以上版本才支持
+        } else {
+            openssl_sign($data, $sign, $res);
+        }
+        $sign = base64_encode($sign);
+        return $sign;
+    }
+
+    /**
+     * 校验$value是否非空
+     *  if not set ,return true;
+     *    if is null , return true;
+     **/
+    protected function checkEmpty($value) {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+
+        return false;
+    }
+
+    public function getSignContent($params) {
+        ksort($params);
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === $this->checkEmpty($v) && "@" != substr($v, 0, 1)) {
+                // 转换成目标字符集
+                $v = $this->characet($v, $this->charset);
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+
+    /**
+     * 转换字符集编码
+     * @param $data
+     * @param $targetCharset
+     * @return string
+     */
+    function characet($data, $targetCharset) {
+        if (!empty($data)) {
+            $fileType = $this->charset;
+            if (strcasecmp($fileType, $targetCharset) != 0) {
+                $data = mb_convert_encoding($data, $targetCharset, $fileType);
+                //$data = iconv($fileType, $targetCharset.'//IGNORE', $data);
+            }
+        }
+        return $data;
+    }
+
+    public function curlPost($url = '', $postData = '', $options = array())
+    {
+        if (is_array($postData)) {
+            $postData = http_build_query($postData);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30); //设置cURL允许执行的最长秒数
+        if (!empty($options)) {
+            curl_setopt_array($ch, $options);
+        }
+        //https请求 不验证证书和host
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
