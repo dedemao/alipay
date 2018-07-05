86c26c0fba1e4a489765bf15d32d5df386ed4bc5
dedemao <admin@dedemao.com>
2018-07-05 14:59:45 +0800

    当面付（扫码支付）

--- qrcode.php
+++ qrcode.php
@@ -21,7 +21,7 @@ $result = $aliPay->doPay();
 $result = $result['alipay_trade_precreate_response'];
 if($result['code'] && $result['code']=='10000'){
     //生成二维码
-    $url = 'http://qr.liantu.com/api.php?text='.$result['qr_code'];
+    $url = 'https://www.kuaizhan.com/common/encode-png?large=true&data='.$result['qr_code'];
     echo "<img src='{$url}' style='width:300px;'><br>";
     echo '二维码内容：'.$result['qr_code'];
 }else{
