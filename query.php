c6fd8bdd676a4c54bc3ddfe3d7a0c246b8a31d2f
dedemao <admin@dedemao.com>
2018-11-28 12:43:28 +0800

    订单状态查询接口

---
+++ query.php
@@ -0,0 +1,197 @@
+<?php
+header('Content-type:text/html; Charset=utf-8');
+/*** 请填写以下配置信息 ***/
+$appid = '';  //https://open.alipay.com 账户中心->密钥管理->开放平台密钥，填写对应应用的APPID
+$outTradeNo = '';     //要查询的商户订单号。注：商户订单号与支付宝交易号不能同时为空
+$tradeNo = '';     //要查询的支付宝交易号。注：商户订单号与支付宝交易号不能同时为空
+$signType = 'RSA2';       //签名算法类型，使用RSA2
+//商户私钥，填写对应签名算法类型的私钥，如何生成密钥参考：https://docs.open.alipay.com/291/105971和https://docs.open.alipay.com/200/105310
+$rsaPrivateKey='';
+/*** 配置结束 ***/
+$aliPay = new AlipayService();
+$aliPay->setAppid($appid);
+$aliPay->setRsaPrivateKey($rsaPrivateKey);
+$aliPay->setOutTradeNo($outTradeNo);
+$aliPay->setTradeNo($tradeNo);
+$result = $aliPay->doQuery();
+if($result['alipay_trade_query_response']['code']!='10000'){
+	echo $result['alipay_trade_query_response']['msg'].'：'.$result['alipay_trade_query_response']['sub_code'].' '.$result['alipay_trade_query_response']['sub_msg'];
+}else{
+	switch($result['alipay_trade_query_response']['trade_status']){
+		case 'WAIT_BUYER_PAY':
+			echo '交易创建，等待买家付款';
+			break;
+		case 'TRADE_CLOSED':
+			echo '未付款交易超时关闭，或支付完成后全额退款';
+			break;
+		case 'TRADE_SUCCESS':
+			echo '交易支付成功';
+			break;
+		case 'TRADE_FINISHED':
+			echo '交易结束，不可退款';
+			break;
+		default:
+			echo '未知状态';
+			break;
+	}
+}
+
+class AlipayService
+{
+    protected $appId;
+    protected $charset;
+    //私钥值
+    protected $rsaPrivateKey;
+    protected $outTradeNo;
+    protected $tradeNo;
+
+    public function __construct()
+    {
+        $this->charset = 'utf8';
+    }
+
+    public function setAppid($appid)
+    {
+        $this->appId = $appid;
+    }
+
+    public function setRsaPrivateKey($saPrivateKey)
+    {
+        $this->rsaPrivateKey = $saPrivateKey;
+    }
+
+    public function setOutTradeNo($outTradeNo)
+    {
+        $this->outTradeNo = $outTradeNo;
+    }
+
+    public function setTradeNo($tradeNo)
+    {
+        $this->tradeNo = $tradeNo;
+    }
+
+    /**
+     * 发起查询
+     * @return array
+     */
+    public function doQuery()
+    {
+        //请求参数
+        $requestConfigs = array(
+            'out_trade_no'=>$this->outTradeNo,
+            'trade_no'=>$this->tradeNo,
+        );
+        $commonConfigs = array(
+            //公共参数
+            'app_id' => $this->appId,
+            'method' => 'alipay.trade.query',             //接口名称
+            'format' => 'JSON',
+            'return_url' => $this->returnUrl,
+            'charset'=>$this->charset,
+            'sign_type'=>'RSA2',
+            'timestamp'=>date('Y-m-d H:i:s'),
+            'version'=>'1.0',
+            'notify_url' => $this->notifyUrl,
+            'biz_content'=>json_encode($requestConfigs),
+        );
+        $commonConfigs["sign"] = $this->generateSign($commonConfigs, $commonConfigs['sign_type']);
+        $result = $this->curlPost('https://openapi.alipay.com/gateway.do',$commonConfigs);
+        return json_decode($result,true);
+    }
+
+   
+    public function generateSign($params, $signType = "RSA") {
+        return $this->sign($this->getSignContent($params), $signType);
+    }
+
+    protected function sign($data, $signType = "RSA") {
+        $priKey=$this->rsaPrivateKey;
+        $res = "-----BEGIN RSA PRIVATE KEY-----\n" .
+            wordwrap($priKey, 64, "\n", true) .
+            "\n-----END RSA PRIVATE KEY-----";
+        ($res) or die('您使用的私钥格式错误，请检查RSA私钥配置');
+        if ("RSA2" == $signType) {
+            openssl_sign($data, $sign, $res, version_compare(PHP_VERSION,'5.4.0', '<') ? SHA256 : OPENSSL_ALGO_SHA256); //OPENSSL_ALGO_SHA256是php5.4.8以上版本才支持
+        } else {
+            openssl_sign($data, $sign, $res);
+        }
+        $sign = base64_encode($sign);
+        return $sign;
+    }
+
+    /**
+     * 校验$value是否非空
+     *  if not set ,return true;
+     *    if is null , return true;
+     **/
+    protected function checkEmpty($value) {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+
+        return false;
+    }
+
+    public function getSignContent($params) {
+        ksort($params);
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === $this->checkEmpty($v) && "@" != substr($v, 0, 1)) {
+                // 转换成目标字符集
+                $v = $this->characet($v, $this->charset);
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+
+    /**
+     * 转换字符集编码
+     * @param $data
+     * @param $targetCharset
+     * @return string
+     */
+    function characet($data, $targetCharset) {
+        if (!empty($data)) {
+            $fileType = $this->charset;
+            if (strcasecmp($fileType, $targetCharset) != 0) {
+                $data = mb_convert_encoding($data, $targetCharset, $fileType);
+            }
+        }
+        return $data;
+    }
+
+    public function curlPost($url = '', $postData = '', $options = array())
+    {
+        if (is_array($postData)) {
+            $postData = http_build_query($postData);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30); //设置cURL允许执行的最长秒数
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('content-type: application/x-www-form-urlencoded;charset=' . $this->charset));
+        if (!empty($options)) {
+            curl_setopt_array($ch, $options);
+        }
+        //https请求 不验证证书和host
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
