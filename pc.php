241fe56676215c0aa37749690a6571100aedc4b8
dedemao <admin@dedemao.com>
2019-03-25 11:25:12 +0800

    电脑网站支付
    兼容php7.2

--- pc.php
+++ pc.php
@@ -111,12 +111,12 @@ class AlipayService
     protected function buildRequestForm($para_temp) {
 
         $sHtml = "正在跳转至支付页面...<form id='alipaysubmit' name='alipaysubmit' action='https://openapi.alipay.com/gateway.do?charset=".$this->charset."' method='POST'>";
-        while (list ($key, $val) = each ($para_temp)) {
+		foreach($para_temp as $key=>$val){
             if (false === $this->checkEmpty($val)) {
                 $val = str_replace("'","&apos;",$val);
                 $sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/>";
-            }
-        }
+            }	
+		}
         //submit按钮控件请不要含有name属性
         $sHtml = $sHtml."<input type='submit' value='ok' style='display:none;''></form>";
         $sHtml = $sHtml."<script>document.forms['alipaysubmit'].submit();</script>";
