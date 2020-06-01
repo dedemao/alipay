f445fc5ccdb343f73ab04945bd83bf9c6d442d0a
dedemao <884358@qq.com>
2020-06-01 13:41:46 +0800

    交易结算（分账、分润）

--- settle.php
+++ settle.php
@@ -44,7 +44,7 @@ class AlipayService
 
     public function __construct()
     {
-        $this->charset = 'utf8';
+        $this->charset = 'utf-8';
     }
 
     public function setAppid($appid)
@@ -116,8 +116,7 @@ class AlipayService
             'biz_content'=>json_encode($requestConfigs),
         );
         $commonConfigs["sign"] = $this->generateSign($commonConfigs, $commonConfigs['sign_type']);
-        $result = $this->curlPost('https://openapi.alipay.com/gateway.do',$commonConfigs);
-        $result = iconv('GBK','UTF-8',$result);
+        $result = $this->curlPost('https://openapi.alipay.com/gateway.do?charset='.$this->charset,$commonConfigs);
         return json_decode($result,true);
     }
     public function generateSign($params, $signType = "RSA") {
