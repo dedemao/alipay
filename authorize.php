851171cb147df3266ee2a02c669f8582c85aa236
dedemao <884358@qq.com>
2020-06-01 13:27:22 +0800

    网页授权获取用户信息

--- authorize.php
+++ authorize.php
@@ -50,7 +50,7 @@ class AlipayService
     protected $auth_code;
     public function __construct()
     {
-        $this->charset = 'utf8';
+        $this->charset = 'utf-8';
     }
 
     public function setAppid($appid)
@@ -92,7 +92,7 @@ class AlipayService
             'code'=>$this->auth_code,
         );
         $commonConfigs["sign"] = $this->generateSign($commonConfigs, $commonConfigs['sign_type']);
-        $result = $this->curlPost('https://openapi.alipay.com/gateway.do',$commonConfigs);
+        $result = $this->curlPost('https://openapi.alipay.com/gateway.do?charset='.$this->charset,$commonConfigs);
         $result = iconv('GBK','UTF-8',$result);
         return json_decode($result,true);
     }
